01892f08bbb5a133a859aa82d66ebdc1662aa7b3
ameeradam2170 <151600957+ameeradam2170@users.noreply.github.com>
2025-01-27 13:14:32 +0530

    Add files via upload

---
+++ rev.js
@@ -0,0 +1,16 @@
+//function mul(a,b){
+//    let c=a*b;
+//    console.log(c);
+//}
+//mul(12,1)
+//let myfun =(a,b)=>{
+ //   let c=a*b;
+//    console.log (c)
+//}; 
+//myfun(5,5)
+  
+let myname =(a,b)=>{
+    let fullname=`My name is ${a} ${b}`
+    console.log(fullname);
+};
+myname("Ameer","adam")   
